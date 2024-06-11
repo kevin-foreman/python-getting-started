0cd6a44b9d096d2d58dacf2c8072945c6c468b96
kevin-foreman <kevin.o.foreman2@gmail.com>
2024-06-11 09:55:18 -0600

    Added a simple python calculator

---
+++ test.py
@@ -0,0 +1,13 @@
+hourly_wage = 20
+
+print('Annual salary is:' )
+print(hourly_wage * 40 * 50)
+print()
+
+print('Monthly salary is: ')
+print(((hourly_wage * 40 * 50) / 12))
+print()
+
+# FIXME: The above is wrong Change
+#        the 1 so that the statement
+#        outputs monthly salary
