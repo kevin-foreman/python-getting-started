521b06fd2920968d49cc1df8b41a45a373426208
kevin-foreman <kevin.o.foreman2@gmail.com>
2024-07-02 08:07:23 -0600

    Added rule sets for player and computer

--- Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors.py
@@ -7,5 +7,23 @@ moves = ["rock", "paper", "scissors"]
 
 # Loop
 while True:
-    computer = moves[randint(0, 2)]
-    
+    computer = moves[randint(0,2)]
+    player = input("rock, paper, or scissors? (or end the game) ").lower()
+    if player == "end the game":
+        print("The game has ended.")
+        break
+    # if player selection matches
+    elif player == computer:
+        print("Tie!")
+    # if player selection is Rock, win, lose, or tie    
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "beats", player)
+        else:
+            print("You win!", player, "beats", computer)
+    # if player selection is Paper, win, lose, or tie        
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "beats", player)
+        else:
+            print("You win!", player, "beats", computer)
